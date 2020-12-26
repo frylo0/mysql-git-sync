499bacb58f60e0a9e7f5ef212e01fb03c132d414
frity <nikonovfedor36936@gmail.com>
2020-12-26 19:48:06 +0300

    v1

---
+++ db-pull.py
@@ -0,0 +1,15 @@
+import os, sys
+
+py = sys.executable
+
+print("""MySQL GIT Pull
+""")
+
+script_dir = os.path.dirname(__file__)
+I = '/' if( os.name == 'posix' )else '\\'
+def dbd(arg):
+   global script_dir
+   os.system(f'{py} .{script_dir}{I}env{I}dbd.py {arg}')
+   
+os.system('git pull')
+dbd('import')
